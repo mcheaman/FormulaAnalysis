c8d21476fda5710ee3cb2d80b22bbfedfecfe8a7
Mason Heaman <mcheaman@yahoo.com>
2024-09-21 20:29:58 -0700

    Spring boot and dependency initialization

---
+++ src/main/java/com/f1telemetry/race_telemetry_analyzer/RaceTelemetryAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.f1telemetry.race_telemetry_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceTelemetryAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaceTelemetryAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/f1telemetry/race_telemetry_analyzer/RaceTelemetryAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f1telemetry.race_telemetry_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceTelemetryAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
