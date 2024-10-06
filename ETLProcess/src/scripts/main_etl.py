7eaea0f977e90b937df2d6b003f72c980dcbcdf8
Mason Heaman <mcheaman@yahoo.com>
2024-10-06 12:54:35 -0700

    ETL pipeline with class components

--- ETLProcess/src/scripts/main_etl.py
+++ ETLProcess/src/scripts/main_etl.py
@@ -1,33 +1,26 @@
 """
-main_etl.py
+maint_etl.py
 
-This module orchestrates the ETL process by calling the extract, transform, and load functions 
-in the correct order. The ETL pipeline extracts data from MongoDB, transforms it into a 
-relational format, and loads it into a PostgreSQL database.
+This script manages the full ETL (Extract, Transform, Load) process, 
+using the Extract, Transform, and Load classes.
 """
 
-from extract import extract
-from transform import transform
-from load import load
+from extract import Extract
+from transform import Transform
+from load import Load
 
 def main():
-    """
-    The main ETL pipeline.
-
-    This function orchestrates the ETL process by extracting data from MongoDB, transforming it into 
-    a relational format, and loading it into PostgreSQL.
-    
-    Returns:
-        None
-    """
     # Step 1: Extract data from MongoDB
-    extracted_data = extract()
+    extractor = Extract()
+    raw_data = extractor.extract()
+     
+    # Step 2: Transform extracted data into a relational form
+    transformer = Transform()
+    transformed_data = transformer.transform(raw_data)
     
-    # Step 2: Transform the extracted data
-    transformed_data = transform(extracted_data)
-    print(transformed_data)
-    # Step 3: Load the transformed data into PostgreSQL
-    load(transformed_data)
+    # Step 3: Load transformed data into PostgreSQL database
+    loader = Load()
+    loader.load(transformed_data)
 
 if __name__ == "__main__":
     main()
